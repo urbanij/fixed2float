e19a509af5bc806e98f805c5228fb70119f5ed35
Francesco Urbani <francescourbanidue@gmail.com>
2022-01-23 21:59:29 +0100

    minor

--- examples/basic.rs
+++ examples/basic.rs
@@ -9,16 +9,9 @@ fn main() {
 
     let bits = 0b1010000010110000;
     let x = to_float(bits, 16, m, n);
-
-    println!("{:?}", to_fixed(10.25, 8, 1));
-
-    println!("{:?}", to_fixed(0.65, 1, 15));
+    println!("{:?}", x);
 
     let x = 10.25;
     let (m, n) = (21, 3);
     println!("{:?}", to_fixed(x, m, n).unwrap().0);
-
-    println!("{:?}", to_float_str("00010011000000100001", 12, 8));
-
-    println!("{:?}", to_fixed(10.25, 8, 2));
 }
