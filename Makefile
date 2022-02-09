426872db308f262da7d30d769de5a5fc3401e740
Francesco Urbani <francescourbanidue@gmail.com>
2022-02-09 17:37:54 +0100

    moved index to docs updated example.py to new api

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 index:
 	cd py-fixed2float/examples && jupyter-nbconvert --to html notebook.ipynb && mv notebook.html .notebook.html
-	ln -sf py-fixed2float/examples/.notebook.html index.html
+	cd docs && ln -sf ../py-fixed2float/examples/.notebook.html index.html
--- py-fixed2float/examples/example.py
+++ py-fixed2float/examples/example.py
@@ -1,9 +1,9 @@
 import fixed2float as f2f
 
-# print(f"{f2f.__version__}")
+print(f"v{f2f.__version__}")
 
-fp1 = f2f.to_fixed(1.25, 4, 3)
-fp2 = f2f.to_fixed(0.25, 4, 3)
+fp1 = f2f.to_Fx(1.25, 4, 7)
+fp2 = f2f.to_Fx(0.25, 4, 7)
 
 
 print(f"{fp1=}, {fp1.val=}, {fp1.eval()=}")
@@ -19,7 +19,8 @@ print(f"{(fp1 >> 2)=}, {(fp1 >> 2).eval()=}")
 
 print(f2f.to_float(0x7F, 8, 2, 6))
 
-print(f2f.to_fixed(1 / 1.4375, 1, 15).val)
+print(f2f.to_Fx(1 / 1.4375, 1, 16).val)
 
 
-fp3 = f2f.FixedPoint(40, 3, 5)
+fp3 = f2f.Fx(40, 3, 8)
+print(fp3)
